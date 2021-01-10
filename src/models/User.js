bb62c3e46ff30d82f468601eca39b02fe41345ba
ChabaJosa <chabagjg@gmail.com>
2021-01-09 22:06:29 -0500

    Fixed jsonwebtoken middleware issue

--- src/models/User.js
+++ src/models/User.js
@@ -1,15 +1,55 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-})
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
 
-mongoose.model('User', userSchema)
+// Pre-save hook, runs before save
+userSchema.pre("save", function (next) {
+  const user = this;
+  // If user hasn't modified their pwd, don't encrypt
+  if (!user.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        next(err);
+      }
+      user.password = hash;
+      // Continues process, saves user
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword) {
+  return new Promise((resolve, reject) => {
+    const user = this;
+    bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+      if (!isMatch) {
+        return reject(false);
+      }
+      // Pwd is a match
+      resolve(true);
+    });
+  });
+};
+
+mongoose.model("User", userSchema);
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -19,4 +19,26 @@ router.post("/signup", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  const {email, password} = req.body;
+  if (!email || !password){
+    return res.status(404).send({error: 'Must provide email and password.(3)'})
+  }
+
+  const user = await User.findOne({email: email})
+
+  if (!user){
+    return res.status(404).send({error: 'Email not found.'})
+  }
+
+  try {
+    await user.comparePassword(password)
+    const token = jwt.sign({userID: user._id}, "MY_SECRET_KEY")
+    res.send({token})
+  } catch (err) {
+    return res.status(404).send({error: 'Invalid pwd or email.'})
+  }
+
+})
+
 module.exports = router;
