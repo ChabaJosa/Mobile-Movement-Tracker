effe60a97e1edc03fbff77c0a65ca9bc020e9f75
ChabaJosa <chabagjg@gmail.com>
2021-01-12 21:30:37 -0500

    Added track post request and testes both post and get with postman, everything seems fine

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -6,14 +6,30 @@ const Track = mongoose.model('Track')
 
 const router = express.Router();
 
-router.use(requireAuth)
 // Everything under this will make use of the requireAuth middleware
+router.use(requireAuth)
 
+// Gets all the tracks for the user ever
 router.get('/tracks', async (req, res) => {
     const tracks = await Track.find({userID : req.user._id})
-    // Gets all the tracks for the user ever
-
     res.send(tracks)
+})
+
+// Posts new tracks
+router.post('/tracks', async (req, res) => {
+    const {name, locations} = req.body
+    if (!name || !locations){
+        return res.status(422).send({error: 'You must provide a name and locations'})
+    }
+
+    try{
+        const track = new Track({name, locations, userID: req.user._id })
+        await track.save()
+        res.send(track)
+    } catch (err) {
+        res.status(422).send({error: err.message})
+    }
+    
 })
 
 module.exports = router;
